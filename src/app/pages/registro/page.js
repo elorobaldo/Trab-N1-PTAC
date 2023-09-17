0c65eb150bf9d981fdbe8de38e2a45f53febf16a
elorobaldo <elo.robaldo1@gmail.com>
2023-09-17 18:19:28 -0300

    regist form

--- src/app/pages/registro/page.js
+++ src/app/pages/registro/page.js
@@ -1,17 +1,28 @@
 'use client'
 import { ToastContainer, toast } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css'
+import '../css/registro.css'
 
 
 export default function Registro() {
   function registro(e) {
     e.preventDefault();
+    toast.success("Deu bom");
   }
   return (
     <div>
      
       <form onSubmit={registro}>
- 
+      <div  className="texto">
+        <p>Nome:</p>
+        <input type="name" required/>
+        <p>Email:</p>
+        <input type="email" required/>
+        <p>Senha</p>
+        <input type="password" required/>
+        <p></p>
+        <button className="btn">Cadastrar</button>
+        </div>
       </form>
       <ToastContainer />
     </div>
