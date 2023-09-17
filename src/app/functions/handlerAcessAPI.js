4aee01ccf1e9ac80105b070c81e5cfbc9dc47edf
elorobaldo <elo.robaldo1@gmail.com>
2023-09-17 20:50:18 -0300

    atualiz

--- src/app/functions/handlerAcessAPI.js
+++ src/app/functions/handlerAcessAPI.js
@@ -1,22 +1,19 @@
 'use server'
-
-const users= [
-
-{
- name:'Stevan', email:'stevan.silva@gmail.com', password:'456',
- token:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
-},
-
-{
- name:'Manoel', email:'manoel.gomes@gmail.com', password:'789',
- token:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
-},
-
-{
- name:'Eloisa', email:'elo.rob@gmail.com', password:'321',
- token:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
-}
-   
+const users = [
+    {
+        name:'Stevan', email:'stevan.silva@gmail.com', password:'456',
+        token:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
+       },
+       
+       {
+        name:'Manoel', email:'manoel.gomes@gmail.com', password:'789',
+        token:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
+       },
+       
+       {
+        name:'Eloisa', email:'elo.rob@gmail.com', password:'321',
+        token:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
+       }
 ]
 
 const getUserAuthenticated = (user) => {
@@ -33,3 +30,4 @@ const getUsers = () =>{
 return users;
 }
 export { getUsers, getUserAuthenticated };
+
